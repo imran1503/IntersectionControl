5f4d301a27f05126ca68925861664f3b7fea8fc6
Imran Latif <imran.latif1503@gmail.com>
2022-10-24 19:19:14 -0400

    First Working

--- first.py
+++ first.py
@@ -1,7 +1,6 @@
 import glob
 import os
 import sys
-import cv2
 import random
 import matplotlib.pyplot as plt
 import time
@@ -9,34 +8,40 @@ import numpy as np
 import argparse
 
 try:
-  sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' %(
-    sys.version_info.major,
-    sys.version_info.minor,
-    'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
 except IndexError:
-  pass
+    pass
 
-import carla 
+import carla
+
 
 def main():
-  actorList = []
-  try:
+    actorList = []
 
-    client = carla.Client('localhost',2000)
+
+    client = carla.Client('localhost', 2000)
     client.set_timeout(10.0)
     world = client.load_world('Town01')
     print(client.get_available_maps())
 
     blueprintLibrary = world.get_blueprint_library()
     vehicle_bp = blueprintLibrary.filter('cybertruck')[0]
-    transform = carla.Transform(carla.Location(x=130,y=195,z=40),carla.Rotation(yaw=0))
-    vehicle = world.spawn_actor(vehicle_bp,transform)
+    transform = carla.Transform(carla.Location(x=200, y=195, z=200), carla.Rotation(yaw=0))
+    vehicle = world.spawn_actor(vehicle_bp, transform)
+    print("Len pre Append" , (len(actorList)))
     actorList.append(vehicle)
+    print("Len post Append" , len(actorList))
 
-  finally:
+    time.sleep(15)
+
+
     print('delete actorList')
-    client.applybatch([carla.command.DestroyActor(x) for x in actorList])
+    if (len(actorList) != 0):
+        client.apply_batch([carla.command.DestroyActor(x) for x in actorList])
 
 
-if _name_ == '_main_':
-  main()
+if __name__ == '__main__':
+    main()
